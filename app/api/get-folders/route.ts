3bd7ebc01051d905690ae3a5dff10e423b5a1058
Alex Straight <compsciaj@outlook.com>
2023-09-14 01:53:21 -0700

    created api for fetching folders

--- app/api/get-folders/route.ts
+++ app/api/get-folders/route.ts
@@ -1,16 +1,26 @@
-import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { prisma } from '@/lib/prisma';
+import { type User } from '@prisma/client';
 
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    return new NextResponse(JSON.stringify({ error: "unauthorized" }), {
+    return new NextResponse(JSON.stringify({ error: 'unauthorized' }), {
       status: 401,
     });
+  } else {
+    const user = session.user as User;
+    if (user && user.id) {
+      const userId = user.id;
+      const folders = await prisma.folder.findMany({
+        where: {
+          id: userId,
+        },
+      });
+      return NextResponse.json({ folders });
+    }
   }
-
-  console.log("GET API", session);
-  return NextResponse.json({ authenticated: !!session });
 }
